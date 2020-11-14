24f042863b5ab9abc9682902c6e822c2825f1e9e
yuumami97 <yuumami97@gmail.com>
2020-11-14 06:42:15 +0000

    init

---
+++ src/main/java/lovesick/firestore/FirestoreApplication.java
@@ -0,0 +1,13 @@
+package lovesick.firestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lovesick/firestore/FirestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lovesick.firestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
