2fa24a26b2b5eded16c3f485516a95b831ce01cc
yuumami97 <yuumami97@gmail.com>
2020-11-14 13:50:44 +0000

    backup

--- src/main/java/lovesick/firestore/FirestoreApplication.java
+++ src/main/java/lovesick/firestore/FirestoreApplication.java
@@ -21,7 +21,7 @@ public class FirestoreApplication {
 
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return String.format("Hello %s!", name);
+    return String.format("Hello, %s", name);
   }
   
   @Bean
